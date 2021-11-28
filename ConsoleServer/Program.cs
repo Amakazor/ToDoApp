fc82842f301ed5c97e279ba25de0f53a5abc982b
PatrykWysocki5140 <estu@onet.pl>
2021-11-28 09:22:40 +0100

    test v2

--- ConsoleServer/Program.cs
+++ ConsoleServer/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleServer
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Arek");
         }
     }
 }
