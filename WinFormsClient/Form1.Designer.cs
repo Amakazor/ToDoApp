954a504b45e0e8e653544d4da077d5efc6a910a5
PatrykWysocki5140 <estu@onet.pl>
2021-12-26 23:13:29 +0100

    Login user form only

--- WinFormsClient/Form1.Designer.cs
+++ WinFormsClient/Form1.Designer.cs
@@ -29,33 +29,102 @@ namespace WinFormsClient
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Connect_button = new System.Windows.Forms.Button();
+            this.Ip_address = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Port = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Status_Info = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // Connect_button
             // 
-            this.button1.Location = new System.Drawing.Point(320, 173);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Connect_button.Location = new System.Drawing.Point(19, 118);
+            this.Connect_button.Name = "Connect_button";
+            this.Connect_button.Size = new System.Drawing.Size(220, 23);
+            this.Connect_button.TabIndex = 0;
+            this.Connect_button.Text = "Connect";
+            this.Connect_button.UseVisualStyleBackColor = true;
+            // 
+            // Ip_address
+            // 
+            this.Ip_address.Location = new System.Drawing.Point(87, 14);
+            this.Ip_address.Name = "Ip_address";
+            this.Ip_address.Size = new System.Drawing.Size(152, 23);
+            this.Ip_address.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP Address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port:";
+            // 
+            // Port
+            // 
+            this.Port.Location = new System.Drawing.Point(87, 50);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(56, 23);
+            this.Port.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Status:";
+            // 
+            // Status_Info
+            // 
+            this.Status_Info.AutoSize = true;
+            this.Status_Info.Location = new System.Drawing.Point(87, 90);
+            this.Status_Info.Name = "Status_Info";
+            this.Status_Info.Size = new System.Drawing.Size(12, 15);
+            this.Status_Info.TabIndex = 6;
+            this.Status_Info.Text = "_";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(255, 154);
+            this.Controls.Add(this.Status_Info);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ip_address);
+            this.Controls.Add(this.Connect_button);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Login User";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Connect_button;
+        private System.Windows.Forms.TextBox Ip_address;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Port;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Status_Info;
     }
 }
 
