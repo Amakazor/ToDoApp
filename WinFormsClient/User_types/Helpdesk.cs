9f0682f0015ac4ef989e13f470bfb5dbbf206508
PatrykWysocki5140 <estu@onet.pl>
2022-01-13 20:29:30 +0100

    Users in client app

---
+++ WinFormsClient/User_types/Helpdesk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsClient.User_types
+{
+    public partial class Helpdesk : Form
+    {
+        public Helpdesk()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WinFormsClient/Users.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using WinFormsClient.User_types;
+
+namespace WinFormsClient
+{
+    public partial class Users : Form
+    {
+        public Users()
+        {
+            InitializeComponent();
+        }
+
+        private void User_Click(object sender, EventArgs e)
+        {
+            User form = new User();
+            form.Show();
+        }
+
+        private void Admin_Click(object sender, EventArgs e)
+        {
+            Admin form = new Admin();
+            form.Show();
+        }
+
+        private void Helpdesk_Click(object sender, EventArgs e)
+        {
+            Helpdesk form = new Helpdesk();
+            form.Show();
+        }
+    }
+}
